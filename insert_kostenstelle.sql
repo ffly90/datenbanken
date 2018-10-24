669e0e4726f4eb868b40be79ad3151f66b7a76e9
Steffen Walter <serenity@mtmedia.org>
2018-10-24 16:14:59 +0200

    inserts

---
+++ insert_kostenstelle.sql
@@ -0,0 +1,5 @@
+INSERT INTO kostenstelle VALUES ('0001','trainee')
+INSERT INTO kostenstelle VALUES ('0002','junior')
+INSERT INTO kostenstelle VALUES ('0003','resident')
+INSERT INTO kostenstelle VALUES ('0004','senior')
+INSERT INTO kostenstelle VALUES ('0005','management')
--- insert_stelle.sql
+++ insert_stelle.sql
@@ -1,1 +1,5 @@
-INSERT INTO stelle VALUES ('00001','Administrator1',
+INSERT INTO stelle VALUES ('00001','Administrator1','0001',1);
+INSERT INTO stelle VALUES ('00002','Administrator2','0002',2);
+INSERT INTO stelle VALUES ('00003','Administrator3','0003',3);
+INSERT INTO stelle VALUES ('00004','Administrator4','0004',4);
+INSERT INTO stelle VALUES ('00005','Administrator5','0005',5);
