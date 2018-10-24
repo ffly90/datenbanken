e5e6c6c6ec6d76bd4d2a7df0d1329a422b7e1bae
autinerd <sidneyjohn23@icloud.com>
2018-10-24 15:59:02 +0200

    removed fucking comma

--- insert_mitarbeiter.sql
+++ insert_mitarbeiter.sql
@@ -1,6 +1,6 @@
 INSERT INTO mitarbeiter (PNR,PFNAME,PVNAME,PANSSTRN,PANSPLZ,PANSORT,PAUSTDAT,PGEBDAT,PGEBNAME,PTITEL,PSTKL,PEINDAT,PKONF,PGEHSUM,PGSTUV,PGMOFREI,STNR)
-VALUES (1,'Maier','Roland','Baustraße 1','30459','Adorf',NULL,'02.04.1988','Müller',NULL,3,'10.12.1997','E',234.67,19.2,20.39,'00002'),
-(2,'Müller','Trude','ABC-Straße 1','09283','Plauen',NULL,'12.05.1897','Müller',NULL,3,'10.12.1997','E',234.67,19.2,20.39,'00003'),
-(3,'Assmann','Lukas','Hollerweg 1','18734','Bheim',NULL,'02.08.2012','Müller',NULL,3,'10.12.1997','E',234.67,19.2,20.39,'00005'),
-(4,'Reich','Caroline','Siemensallee 1','75862','Weiler',NULL,'','Müller',NULL,3,'10.12.1997','E',234.67,19.2,20.39,'00001'),
-(5,'Specht','Stefan','Wesselyring 1','52836','Fessenheim',NULL,'02.04.1988','Müller',NULL,3,'10.12.1997','E',234.67,19.2,20.39,'00004');
+VALUES (1,'Maier','Roland','Baustraße 1','30459','Adorf',NULL,'02.04.1988','Müller',NULL,3,'10.12.1997','E',2342.67,19.2,20.39,'00002'),
+(2,'Müller','Trude','ABC-Straße 1','09283','Plauen',NULL,'12.05.1897','Müller',NULL,1,'11.12.1997','E',575.67,19.2,20.39,'00003'),
+(3,'Assmann','Lukas','Hollerweg 1','18734','Bheim',NULL,'02.08.2012','Müller',NULL,5,'12.12.1997','E',231.67,19.2,20.39,'00005'),
+(4,'Reich','Caroline','Siemensallee 1','75862','Weiler',NULL,'23.04.1977','Müller',NULL,4,'13.12.1997','E',5421.67,19.2,20.39,'00001'),
+(5,'Specht','Stefan','Wesselyring 1','52836','Fessenheim',NULL,'02.04.1989','Müller',NULL,2,'14.12.1997','E',6632.67,19.2,20.39,'00004');
