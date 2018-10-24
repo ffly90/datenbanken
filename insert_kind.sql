6f475c005b692304992b11a524ff52dd22ba5c2a
autinerd <sidneyjohn23@icloud.com>
2018-10-24 16:13:36 +0200

    insert king, tätigkeit

---
+++ insert_kind.sql
@@ -0,0 +1,5 @@
+INSERT INTO kind values (1, '01.01.2017', 'Jeremy-Pascal',  'w'),
+(1, '01.12.2017', 'Jacqueline-Rualita', 'w'),
+(3, '23.05.1997', 'Lukas', 'm'),
+(5, '28.02.2003', 'Delilah', 'w'),
+(2, '10.10.2001', 'Roger', 'm');
